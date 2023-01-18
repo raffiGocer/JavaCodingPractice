73bfa1caa9731d5ce3f257fcd8f319e172059fb1
RaffiGocer <raffigocer@gmail.com>
2023-01-18 17:32:44 -0500

    Created initial files

--- src/week3/PrimeChecker.java
+++ src/week3/PrimeChecker.java
@@ -3,28 +3,30 @@ package week3;
 public class PrimeChecker {
 
 
-
-
-        public static boolean isPrime(int num) {
-            if (num <= 1) {
+    // This code checks if a given number is prime
+    public static boolean isPrime(int num) {
+        // return false for numbers less than or equal to 1
+        if (num <= 1) {
+            return false;
+        }
+        // check if the number is divisible by any number between 2 and itself
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 return false;
             }
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    return false;
-                }
-            }
-            return true;
         }
-
-        public static void main(String[] args) {
-            int num = 37;
-            if (isPrime(num)) {
-                System.out.println(num + " is a prime number.");
-            } else {
-                System.out.println(num + " is not a prime number.");
-            }
-        }
+        // if the number is not divisible by any number, it is prime
+        return true;
     }
 
+    public static void main(String[] args) {
+        int num = 37;
+        // check if the given number is prime
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
 
--- src/week3/ReverseNegativeNumber.java
+++ src/week3/ReverseNegativeNumber.java
@@ -4,38 +4,28 @@ public class ReverseNegativeNumber {
 
     public static void main(String[] args) {
 
-        int num = -9;
-        String str = Integer.toString(num);
-        String reversedStr = new StringBuilder(str).reverse().toString();
-        int reversedNum = Integer.parseInt(reversedStr);
-        System.out.println(reversedNum); // Output: -5
-
+        System.out.println(reverseNegativeNumber(-321));
     }
 
-/*
-        public static int reverse(int number) {
-            int reversed = 0;
-            boolean isNegative = false; //check if the number is negative
-            if (number < 0) {
-                isNegative = true;
-                number = -number; //if the number is negative make it positive
+    // Method to reverse a negative number and return it as int
+    public static int reverseNegativeNumber(int num) {
+        // Check if the number is negative
+        if (num < 0) {
+            int reversedNum = 0;
+            int temp = -num;
+            // Iterate through each digit of the number
+            while (temp > 0) {
+                reversedNum = reversedNum * 10 + temp % 10;
+                temp /= 10;
             }
-            while (number != 0) {
-                reversed = reversed * 10 + number % 10; // Get the last digit of the number and
-                // add it to reversed number
-                number /= 10; //remove the last digit from the number
-            }
-            return isNegative ? -reversed : reversed; // return the reversed number with the appropriate sign
+            // Add the negative sign back to the reversed number
+            reversedNum = -reversedNum;
+            // Return the reversed number
+            return reversedNum;
         }
-        public static void main(String[] args) {
-            int number = -123;
-            int reversed = reverse(number);
-            System.out.println("The reversed number is: " + reversed);
+        // If the number is not negative, return the original number
+        return num;
+    }
 
-
- */
-
-        }
-
-
+}
 
